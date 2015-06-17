90fa91e12250718d53c0a1c438437bef98bc6f41
Tom Preston-Werner <tom@mojombo.com>
2015-06-16 17:31:42 -0700

    Update to use 4.0.5 kernel.

--- profiles/devices/acer-c720/scripts/kernel.sh
+++ profiles/devices/acer-c720/scripts/kernel.sh
@@ -3,8 +3,8 @@
 tempbuild=`mktemp -d`
 cd $tempbuild
 
-wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.17.6-vivid/linux-headers-3.17.6-031706_3.17.6-031706.201412071535_all.deb
-wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.17.6-vivid/linux-headers-3.17.6-031706-generic_3.17.6-031706.201412071535_amd64.deb
-wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v3.17.6-vivid/linux-image-3.17.6-031706-generic_3.17.6-031706.201412071535_amd64.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.0.5-wily/linux-headers-4.0.5-040005_4.0.5-040005.201506061639_all.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.0.5-wily/linux-headers-4.0.5-040005-generic_4.0.5-040005.201506061639_amd64.deb
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.0.5-wily/linux-image-4.0.5-040005-generic_4.0.5-040005.201506061639_amd64.deb
 
 sudo dpkg -i *.deb
