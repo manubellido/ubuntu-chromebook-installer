3b9ef6112944448aaace3c148e44c054a438b628
manubellido <manubellido@gmail.com>
2015-07-28 22:58:08 -0700

    Update xcodestarter.sh

--- profiles/devices/acer-c720/scripts/xcodestarter.sh
+++ profiles/devices/acer-c720/scripts/xcodestarter.sh
@@ -12,80 +12,80 @@ dpkg -i google-chrome-stable_current_amd64.deb
 export DEBIAN_FRONTEND=noninteractive; apt-get -f -y -q install
 touch "chrome.done"
 
-echo "Installing Scratch"
-cd $tempbuild
-export DEBIAN_FRONTEND=noninteractive; apt-get -y -q install scratch
-touch "scratch.done"
+# echo "Installing Scratch"
+# cd $tempbuild
+# export DEBIAN_FRONTEND=noninteractive; apt-get -y -q install scratch
+# touch "scratch.done"
 
-echo "Installing Geany"
-cd $tempbuild
-export DEBIAN_FRONTEND=noninteractive; apt-get -y -q install geany geany-plugins
-touch "geany.done"
+# echo "Installing Geany"
+# cd $tempbuild
+# export DEBIAN_FRONTEND=noninteractive; apt-get -y -q install geany geany-plugins
+# touch "geany.done"
 
 echo "Installing Vim"
 cd $tempbuild
 export DEBIAN_FRONTEND=noninteractive; apt-get -y -q install vim
 touch "vim.done"
 
-echo "Installing Emacs"
-cd $tempbuild
-export DEBIAN_FRONTEND=noninteractive; apt-get -y -q install emacs
-touch "emacs.done"
+# echo "Installing Emacs"
+# cd $tempbuild
+# export DEBIAN_FRONTEND=noninteractive; apt-get -y -q install emacs
+# touch "emacs.done"
 
-echo "Installing ruby-install"
-cd $tempbuild
-wget -O ruby-install-0.4.3.tar.gz https://github.com/postmodern/ruby-install/archive/v0.4.3.tar.gz
-tar -xzf ruby-install-0.4.3.tar.gz
-cd ruby-install-0.4.3/
-make install
-cd $tempbuild
-touch "ruby-install.done"
+# echo "Installing ruby-install"
+# cd $tempbuild
+# wget -O ruby-install-0.4.3.tar.gz https://github.com/postmodern/ruby-install/archive/v0.4.3.tar.gz
+# tar -xzf ruby-install-0.4.3.tar.gz
+# cd ruby-install-0.4.3/
+# make install
+# cd $tempbuild
+# touch "ruby-install.done"
 
-echo "Installing ruby"
-cd $tempbuild
-ruby-install ruby 2.1.2
-touch "ruby.done"
+# echo "Installing ruby"
+# cd $tempbuild
+# ruby-install ruby 2.1.2
+# touch "ruby.done"
 
-echo "Installing chruby"
-cd $tempbuild
-wget -O chruby-0.3.8.tar.gz https://github.com/postmodern/chruby/archive/v0.3.8.tar.gz
-tar -xzf chruby-0.3.8.tar.gz
-cd chruby-0.3.8/
-make install
-echo "" >> /etc/skel/.bashrc
-echo "source '/usr/local/share/chruby/chruby.sh'" >> /etc/skel/.bashrc
-echo "source '/usr/local/share/chruby/auto.sh'" >> /etc/skel/.bashrc
-echo "chruby ruby-2.1.2" >> /etc/skel/.bashrc
-cd $tempbuild
-touch "chruby.done"
+# echo "Installing chruby"
+# cd $tempbuild
+# wget -O chruby-0.3.8.tar.gz https://github.com/postmodern/chruby/archive/v0.3.8.tar.gz
+# tar -xzf chruby-0.3.8.tar.gz
+# cd chruby-0.3.8/
+# make install
+# echo "" >> /etc/skel/.bashrc
+# echo "source '/usr/local/share/chruby/chruby.sh'" >> /etc/skel/.bashrc
+# echo "source '/usr/local/share/chruby/auto.sh'" >> /etc/skel/.bashrc
+# echo "chruby ruby-2.1.2" >> /etc/skel/.bashrc
+# cd $tempbuild
+# touch "chruby.done"
 
-echo "Installing node.js & npm"
-cd $tempbuild
-export DEBIAN_FRONTEND=noninteractive; apt-get -y -q install nodejs npm
-touch "nodejs.done"
+# echo "Installing node.js & npm"
+# cd $tempbuild
+# export DEBIAN_FRONTEND=noninteractive; apt-get -y -q install nodejs npm
+# touch "nodejs.done"
 
 echo "Installing Java"
 cd $tempbuild
 export DEBIAN_FRONTEND=noninteractive; apt-get -y -q install default-jdk
 touch "java.done"
 
-echo "Installing Minecraft"
-cd $tempbuild
-export DEBIAN_FRONTEND=noninteractive; add-apt-repository -y ppa:minecraft-installer-peeps/minecraft-installer
-export DEBIAN_FRONTEND=noninteractive; apt-get -y -q update
-export DEBIAN_FRONTEND=noninteractive; apt-get -y -q install minecraft-installer
-touch "minecraft.done"
+# echo "Installing Minecraft"
+# cd $tempbuild
+# export DEBIAN_FRONTEND=noninteractive; add-apt-repository -y ppa:minecraft-installer-peeps/minecraft-installer
+# export DEBIAN_FRONTEND=noninteractive; apt-get -y -q update
+# export DEBIAN_FRONTEND=noninteractive; apt-get -y -q install minecraft-installer
+# touch "minecraft.done"
 
-echo "Installing Git"
-cd $tempbuild
-export DEBIAN_FRONTEND=noninteractive; apt-get -y -q install git
-touch "git.done"
+# echo "Installing Git"
+# cd $tempbuild
+# export DEBIAN_FRONTEND=noninteractive; apt-get -y -q install git
+# touch "git.done"
 
-echo "Installing wallpaper"
-cd $tempbuild
-wget https://s3-us-west-1.amazonaws.com/mojombo-codestarter/codestarter-tree.jpg
-cp codestarter-tree.jpg /usr/share/backgrounds
-touch "wallpaper.done"
+# echo "Installing wallpaper"
+# cd $tempbuild
+# wget https://s3-us-west-1.amazonaws.com/mojombo-codestarter/codestarter-tree.jpg
+# cp codestarter-tree.jpg /usr/share/backgrounds
+# touch "wallpaper.done"
 
 # These dconf overrides do the following:
 # 1. Customizes the Launcher (left dock bar) to hold commonly used programs.
@@ -95,29 +95,12 @@ echo "Installing dconf overrides"
 cd $tempbuild
 echo -e "[com.canonical.Unity.Launcher]\n\
 \n\
-favorites=['application://nautilus.desktop', 'application://google-chrome.desktop', 'application://geany.desktop', 'application://gnome-terminal.desktop', 'application://minecraft.desktop', 'application://ubuntu-software-center.desktop', 'application://unity-control-center.desktop', 'unity://running-apps']
+favorites=['application://nautilus.desktop', 'application://google-chrome.desktop', 'application://gnome-terminal.desktop', 'application://ubuntu-software-center.desktop', 'application://unity-control-center.desktop', 'unity://running-apps']
 \n\
 [org.gnome.settings-daemon.peripherals.touchpad]\n\
 \n\
-natural-scroll=true\n\
-\n\
-[org.gnome.desktop.background]\n\
-\n\
-picture-uri='file:///usr/share/backgrounds/codestarter-tree.jpg'" > /usr/share/glib-2.0/schemas/codestarter.gschema.override
 glib-compile-schemas /usr/share/glib-2.0/schemas/
 touch "dconf-overrides.done"
-
-# Add registration shortcut to desktop
-serial=$(cat /etc/serialnumber)
-mkdir -p /etc/skel/Desktop
-cat <<EOF > /etc/skel/Desktop/register.desktop
-[Desktop Entry]
-Encoding=UTF-8
-Name=Register your Codestarter Laptop
-Type=Link
-URL=https://codestarter.org/start?serial=$serial
-Icon=register-48
-EOF
 
 # Cleanup
 rm -fr /tmp/tmp.*
